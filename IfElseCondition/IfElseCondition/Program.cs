fc45a32de983bab0ffc9368a51ee0b2eb4813e82
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-09 19:10:44 +0530

    If else condition

---
+++ IfElseCondition/IfElseCondition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            if (age > 18)
+            {
+                Console.WriteLine("Eligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible for voting");
+            }
+        }
+    }
+}
